7159561947a3ace9c80214eaefce2deb9cbfd848
owtotwo <806425292@qq.com>
2016-03-12 15:28:22 +0800

    update

--- WishGroup/app/__init__.py
+++ WishGroup/app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config')
+app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
+db = SQLAlchemy(app)
 
+from flask.ext import login
+login_manager = login.LoginManager()
+login_manager.setup_app(app)
+
+from app import view
--- WishGroup/flask/bin/activate.fish
+++ WishGroup/flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/at/WishGroup/flask"
+set -gx VIRTUAL_ENV "/home/at/test/microblog/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
